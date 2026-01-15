76ae4c02e25839878218ed1470cb23f75f09e235
dandelion <1993466159@qq.com>
2026-01-16 02:17:35 +0800

    init

---
+++ src/entries/content/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">app</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#app {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+}
+</style>
